6e31716ccf950c5630728aea7e708b132bf5889c
Vit Petrik <petrivi2@fel.cvut.cz>
2023-04-15 10:50:11 +0200

    Kinda poorly performing artificial potential field

--- src/leader_follower.cpp
+++ src/leader_follower.cpp
@@ -13,6 +13,7 @@
 #include <mrs_msgs/SpeedTrackerCommand.h>
 #include <mrs_msgs/String.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <algorithm>
 
 #include "leader_follower.h"
 #include <ros/ros.h>
@@ -23,6 +24,24 @@
 #define GOAL_DISTANCE 5.0
 
 ros::Publisher force_pub;
+ros::ServiceClient switch_ser;
+
+
+double attractive_force(double distance, int order)
+{
+    if (distance < GOAL_DISTANCE)
+        return 0;
+
+    return (1/order)*pow(1/distance - 1/GOAL_DISTANCE , order);
+}
+
+double repulsive_force(double distance, int order)
+{
+    if (distance >= GOAL_DISTANCE)
+        return 0;
+
+    return (1/order)*pow(1/distance - 1/GOAL_DISTANCE , order);
+}
 
 void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
 {
@@ -30,59 +49,77 @@ void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
     cmd.header.stamp = ros::Time::now();
     cmd.header.frame_id = msg.header.frame_id;
 
-    Eigen::Vector3d force = Eigen::Vector3d::Zero();
+    Eigen::Vector3d acceleration = Eigen::Vector3d::Zero();
 
     for (auto element : msg.poses)
     {
+        if(element.id != 0x0a)
+            continue;
         Eigen::Vector3d body(element.pose.position.x, element.pose.position.y, element.pose.position.z);
         double distance = body.norm();
         body /= distance;
 
-        double attractive_force = 5*pow(distance - GOAL_DISTANCE, 2);
-        double repulsive_force = -50*(1 - 1/pow(distance/GOAL_DISTANCE, 2));
+        ROS_INFO_STREAM("[LEADER_FOLLOWER] Distance: " << distance);
+
+        double attractive_force = 0.01*pow(distance - GOAL_DISTANCE, 2);
+        double repulsive_force = 0.01*(1 - 1/pow(distance/GOAL_DISTANCE, 2));
+
+        ROS_INFO_STREAM("[LEADER_FOLLOWER] repulsive_force: " << repulsive_force);
 
         if (distance >= GOAL_DISTANCE)
-            force += attractive_force * body;
+            acceleration += attractive_force * body;
         else
-            force += repulsive_force * body;
+            acceleration += repulsive_force * body;
+
     }
 
-    cmd.force.x = force(0);
-    cmd.force.y = force(1);
-    cmd.force.z = force(2);
-    cmd.use_force = true;
+    ROS_INFO_STREAM("[LEADER_FOLLOWER] Force: " << acceleration.transpose());
+
+    cmd.acceleration.x = acceleration(0);
+    cmd.acceleration.y = acceleration(1);
+    cmd.acceleration.z = 0;
+    cmd.heading = atan2(acceleration(1), acceleration(0));
+    cmd.use_heading = true;
+    cmd.height = 5;
+    cmd.use_acceleration = true;
+    cmd.use_height = true;
 
     force_pub.publish(cmd);
+    mrs_msgs::String tracker;
+    tracker.request.value = "SpeedTracker";
+    bool status = switch_ser.call(tracker);
+
+    return;
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "leader_follower");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
 
-    ros::Subscriber pose_sub = nh.subscribe("~leader", 10, leader_cb);
-    force_pub = nh.advertise<mrs_msgs::PoseWithCovarianceArrayStamped>("~force", 10);
+    ros::Subscriber pose_sub = nh.subscribe("leader", 10, leader_cb);
+    force_pub = nh.advertise<mrs_msgs::SpeedTrackerCommand>("force", 10);
 
-    ros::ServiceClient client = nh.serviceClient<mrs_msgs::String>("~switch_tracker");
+    switch_ser = nh.serviceClient<mrs_msgs::String>("switch_tracker");
 
-    mrs_msgs::SpeedTrackerCommand cmd;
-    cmd.header.stamp = ros::Time::now();
-    cmd.header.frame_id = "fcu";
-    cmd.velocity.x = 0;
-    cmd.velocity.y = 0;
-    cmd.velocity.z = 0;
-    cmd.force.x = 0;
-    cmd.force.y = 0;
-    cmd.force.z = 0;
-    cmd.use_force = true;
-    cmd.use_velocity = false;
+    // mrs_msgs::SpeedTrackerCommand cmd;
+    // cmd.header.stamp = ros::Time::now();
+    // cmd.header.frame_id = "fcu";
+    // cmd.velocity.x = 0;
+    // cmd.velocity.y = 0;
+    // cmd.velocity.z = 0;
+    // cmd.acceleration.x = 0;
+    // cmd.acceleration.y = 0;
+    // cmd.acceleration.z = 0;
+    // cmd.use_acceleration = true;
+    // cmd.use_velocity = true;
 
-    force_pub.publish(cmd);
-    mrs_msgs::String tracker;
-    tracker.request.value = "SpeedTracker";
-    bool status = client.call(tracker);
+    // force_pub.publish(cmd);
+    // mrs_msgs::String tracker;
+    // tracker.request.value = "SpeedTracker";
+    // bool status = switch_ser.call(tracker);
 
-    ROS_INFO("[LEADER_FOLLOWER] Switched to SpeedTracker: %d", status);
+    // ROS_INFO("[LEADER_FOLLOWER] Switched to SpeedTracker: %d", status);
 
     ros::spin();
     return 0;
