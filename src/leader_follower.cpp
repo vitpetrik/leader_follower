af56e41a578b05c61af0b8e2ba617335d47ada9d
Vit Petrik <petrivi2@fel.cvut.cz>
2023-04-23 14:07:25 +0200

    Most of the edge cases solved

--- src/leader_follower.cpp
+++ src/leader_follower.cpp
@@ -121,7 +121,7 @@ double angdiff(double th1, double th2)
 {
     double d = 0;
     d = th1 - th2;
-    d = std::fmod(d+M_PI, 2*M_PI) - M_PI;
+    d = std::fmod(d + M_PI, 2 * M_PI) - M_PI;
     return d;
 }
 
@@ -166,12 +166,8 @@ void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
         heading = atan2(leader_pos(1), leader_pos(0));
         // heading = tf::getYaw(q_tf);
 
-        ROS_INFO_STREAM("[LEADER FOLLOWER] heading: " << heading);
-
         Eigen::Vector3d follower = leader_q.inverse() * (-leader_pos);
         follower(2) = 0;
-
-        ROS_INFO_STREAM("[LEADER FOLLOWER] follower: " << follower.transpose());
 
         target_pos << distance, 0, 0;
         target_pos = target_q * target_pos;
@@ -179,11 +175,8 @@ void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
         Eigen::Vector3d follower_tp = follower - target_pos;
         follower_tp = target_q.inverse() * follower_tp;
 
-        ROS_INFO_STREAM("[LEADER FOLLOWER] follower_tp: " << follower_tp.transpose());
-
-        if (follower_tp(0) >= -0.5)
+        if (follower_tp(0) >= -1.0 or follower_tp.norm() < 1)
         {
-            ROS_INFO("[LEADER FOLLOWER] No need to do tangent stupid things");
             Eigen::Vector3d new_position = leader_q * target_pos;
             new_position(2) = 0;
 
@@ -197,52 +190,21 @@ void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
             point.position.y = new_position(1);
             point.position.z = new_position(2);
 
-            point.heading = heading;
+            point.heading = atan2(new_position(1) - leader_pos(1), new_position(0) - leader_pos(0)) + M_PI;
 
             path.request.path.points.push_back(point);
         }
-        else
+        else if (follower.norm() < distance + 1)
         {
+            double current_angle = atan2(follower(1), follower(0));
             double final_angle = M_PI * angle / 180;
+            double angle_increment = angdiff(final_angle, current_angle) / 20;
 
-            double theta = acos(distance / follower.norm());
-            double beta = atan2(follower(1), follower(0));
-
-            // beta = beta < 0 ? beta + 2*M_PI : beta;
-
-            ROS_INFO_STREAM("[LEADER FOLLOWER] theta: " << theta);
-            ROS_INFO_STREAM("[LEADER FOLLOWER] beta: " << beta);
-
-            double d1 = angdiff(-(theta + beta), 0);
-            double d2 = angdiff(-(theta - beta), 0);
-
-            ROS_INFO_STREAM("[LEADER FOLLOWER] d1: " << d1);
-            ROS_INFO_STREAM("[LEADER FOLLOWER] d2: " << d2);
-
-            double diff1 = angdiff(final_angle, d1);
-            double diff2 = angdiff(final_angle, d2);
-
-            ROS_INFO_STREAM("[LEADER FOLLOWER] diff1: " << diff1);
-            ROS_INFO_STREAM("[LEADER FOLLOWER] diff2: " << diff2);
-
-            double preffered_angle = 0;
-
-            if (abs(diff1) <= abs(diff2))
-                preffered_angle = d1;
-            else
-                preffered_angle = d2;
-
-            ROS_INFO_STREAM("[LEADER FOLLOWER] preffered_angle: " << preffered_angle << " " << final_angle);
-
-            double angle_increment = angdiff(final_angle, preffered_angle) / 20;
-
-            ROS_INFO_STREAM("[LEADER_FOLLOWER] Preffered angle: " << preffered_angle);
-
-            for (int i = 0; i <= 20; i++)
+            for (int i = 1; i <= 20; i++)
             {
                 Eigen::Quaterniond q_circle = Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX()) *
                                               Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY()) *
-                                              Eigen::AngleAxisd(preffered_angle + i * angle_increment, Eigen::Vector3d::UnitZ());
+                                              Eigen::AngleAxisd(current_angle + i * angle_increment, Eigen::Vector3d::UnitZ());
 
                 Eigen::Vector3d circle_pos = Eigen::Vector3d::Zero();
                 circle_pos << 1, 0, 0;
@@ -258,7 +220,11 @@ void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
 
                 new_position += leader_pos;
 
-                ROS_INFO_STREAM("[LEADER_FOLLOWER] Follower: " << new_position.transpose());
+                if ((leader_pos - new_position).norm() < distance - 0.1)
+                {
+                    ROS_INFO_STREAM("[LEADER FOLLOWER] Something really cursed happend");
+                    return;
+                }
 
                 mrs_msgs::Reference point;
                 point.position.x = new_position(0);
@@ -270,6 +236,68 @@ void leader_cb(const mrs_msgs::PoseWithCovarianceArrayStamped &msg)
                 path.request.path.points.push_back(point);
             }
         }
+        else
+        {
+            double final_angle = M_PI * angle / 180;
+
+            double theta = acos(distance / follower.norm());
+            double beta = atan2(follower(1), follower(0));
+
+            double d1 = angdiff(-(theta + beta), 0);
+            double d2 = angdiff(-(theta - beta), 0);
+
+            double diff1 = angdiff(final_angle, d1);
+            double diff2 = angdiff(final_angle, d2);
+
+            double preffered_angle = 0;
+
+            if (abs(diff1) <= abs(diff2))
+                preffered_angle = d1;
+            else
+                preffered_angle = d2;
+
+            Eigen::Quaterniond q_circle = Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX()) *
+                                          Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY()) *
+                                          Eigen::AngleAxisd(preffered_angle, Eigen::Vector3d::UnitZ());
+
+            Eigen::Vector3d circle_pos = Eigen::Vector3d::Zero();
+            circle_pos << 1, 0, 0;
+            circle_pos = q_circle * circle_pos;
+
+            Eigen::Vector3d new_position = leader_q * circle_pos;
+
+            new_position(2) = 0;
+            new_position.normalize();
+            new_position *= distance;
+
+            new_position += leader_pos;
+
+            Eigen::Vector3d line_increment = (new_position) / 10;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Eigen::Vector3d new_position = line_increment * i;
+
+                if ((leader_pos - new_position).norm() < distance - 0.1)
+                {
+                    ROS_INFO_STREAM("[LEADER FOLLOWER] Something really cursed happend");
+                    return;
+                }
+                heading = atan2(leader_pos(1) - new_position(1), leader_pos(0) - new_position(0));
+
+                mrs_msgs::Reference point;
+                point.position.x = new_position(0);
+                point.position.y = new_position(1);
+                point.position.z = new_position(2);
+
+                point.heading = heading;
+
+                path.request.path.points.push_back(point);
+            }
+        }
+
+        // sanity check
+
         path_ser.call(path);
         // running = false;
     }
@@ -292,6 +320,12 @@ bool start(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
     return true;
 }
 
+bool goto_position(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
+{
+    running = true;
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "leader_follower");
@@ -305,6 +339,7 @@ int main(int argc, char **argv)
 
     ros::ServiceServer start_follower = nh.advertiseService("start", start);
     ros::ServiceServer stop_follower = nh.advertiseService("stop", stop);
+    ros::ServiceServer goto_position_follower = nh.advertiseService("goto_position", goto_position);
 
     ros::Subscriber pose_sub = nh.subscribe("leader", 10, leader_cb);
     force_pub = nh.advertise<mrs_msgs::SpeedTrackerCommand>("force", 10);
